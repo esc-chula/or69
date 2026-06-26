ba1fe635f42c90aee48c5b0c03f54290bedd6daf
Patthadon Phengpinij <patthadon.1807@gmail.com>
2026-06-26 15:48:48 +0700

    feat: upload new version of calendar images (agenda and calendar)

--- src/config/calendar-posts.ts
+++ src/config/calendar-posts.ts
@@ -3,8 +3,8 @@ import { cohort } from '@/config/cohort';
 import CalendarCarousel1 from '@/assets/images/calendar/68_openchat1.jpg';
 import CalendarCarousel2 from '@/assets/images/calendar/68_openchat2.jpg';
 import CalendarCarousel3 from '@/assets/images/calendar/68_openchat3.jpg';
-import activityImage from '@/assets/images/calendar/68_calendar.png';
-import registrationImage from '@/assets/images/calendar/68_agenda.jpg';
+import activityImage from '@/assets/images/calendar/69_calendar.jpg';
+import registrationImage from '@/assets/images/calendar/69_agenda.jpg';
 
 export interface CalendarPost {
     instagramUrl: string;
@@ -30,8 +30,7 @@ export const calendarPosts: CalendarPost[] = [
         },
     },
     {
-        instagramUrl:
-            'https://www.instagram.com/p/DLPGG6DSFKV/?igsh=MWU0N3dtdG9zaG5uNw==',
+        instagramUrl: 'https://www.instagram.com/p/DZ4UYDEElbb/?img_index=1',
         images: [activityImage],
         scrollable: false,
         className: 'h-full w-full items-center text-base',
@@ -40,8 +39,7 @@ export const calendarPosts: CalendarPost[] = [
             `ชาว Intania ${cohort.intania} ทุกคนจะได้เจอกับกิจกรรมต่างมากมายและพบเพื่อนทั้งในและนอกคณะ บอกเลยว่าห้ามพลาดทุกกิจกรรมเลย !`,
     },
     {
-        instagramUrl:
-            'https://www.instagram.com/p/DLPIWbDSS4T/?igsh=NmFwc2c2cWd0eHJn',
+        instagramUrl: 'https://www.instagram.com/p/DZ4UYDEElbb/?img_index=2',
         images: [registrationImage],
         scrollable: false,
         caption: `📣 กำหนดการการลงทะเบียนนิสิตใหม่ ภาคการศึกษาต้น ปีการศึกษา ${cohort.academicYearTH} ⚙️📚 [รหัส ${cohort.batch}]`,
